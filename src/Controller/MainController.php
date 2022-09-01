af8ed7b036bf3ec76181d5e2d24af0fe9dc1d35c
olivier <olivier.catanese@gmail.com>
2022-09-01 10:35:54 +0200

    ajout base.html.twig

---
+++ src/Controller/MainController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+//    /**
+//     * @Route("/", name="app_home")
+//     */
+//    public function index(): Response
+//    {
+//        return $this->render('home/index.html.twig', [
+//            'controller_name' => 'MainController',
+//        ]);
+//    }
+
+
+//
+//    public function home(){
+//        return $this->render(home/index.html.twig);
+//    }
+
+
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function home(){
+        return $this->render('home/index.html.twig');
+    }
+
+
+}
